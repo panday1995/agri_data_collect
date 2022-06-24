e88d01bb2db7427687cbb50882ef45421c819fd6
Cmmm丶 <49300691+panday1995@users.noreply.github.com>
2022-06-24 14:17:53 +0200

    Update nutri_balance.py

--- agri_mass_balance/nutri_balance.py
+++ agri_mass_balance/nutri_balance.py
@@ -3,12 +3,11 @@ Created on Jun 20 2022
 @author: Fan Yang
 
 ToDo
+- distribute nutrient across faostat harvest area
 - generate .csv file that store nitrogen balance for crops
 
 """
 
 import utilities_agri as ua
-
-
 
 ua.read_fert_input()
